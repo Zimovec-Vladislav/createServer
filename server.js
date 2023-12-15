ece4a972018e2f03d1cfbdbb5eab28bb7232b907
Vladislav <boss.zimovet@gmail.com>
2023-12-15 16:59:32 +0300

    3 train

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http')
 
-const server = http.createServer((res, req) => {
-    req.write('Communizm suck')
-    req.end()
-}).listen(3000)
+const server = http.createServer((req, res) =>{
+    res.write('Its answer to client. ')
+    res.end('Its the end')
+})
+
+server.listen(3000)

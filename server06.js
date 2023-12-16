91cb2ed05256a3e9a6effd909ffb0b6320f59a8e
Vladislav <boss.zimovet@gmail.com>
2023-12-16 23:32:18 +0300

    final train

--- server06.js
+++ server06.js
@@ -1,22 +1,24 @@
 const http = require('http')
 
-const server = http.createServer((req, res) =>{
-
+const server = http.createServer((req, res) => {
+    
     switch (req.url) {
         
         case '/home' : {
             setTimeout(() => {
-                const data = "It's possible"
+                const data = 'Sell'
                 res.write(data)
                 res.end()
             },5000)
             break
-        } 
+        }
+
         default : {
-            res.write('404 not found')
+            res.write("404 fuck off")
             res.end()
         }
+        
     }
 })
 
-server.listen(3000)
+server.listen(3003)
